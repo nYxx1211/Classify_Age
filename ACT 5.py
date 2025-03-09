993b7ef364cf387f6d1c080192c02b09aa21f9de
Mark Joseph Aquino <nyxx1225@gmail.com>
2025-03-09 21:34:41 +0800

    Add files via upload

---
+++ ACT 5.py
@@ -0,0 +1,13 @@
+def classify_age(age):
+    if age < 0:
+        print("Invalid age. Please enter a non-negative number.")
+    elif age <=12:
+        print("You are a Child.")
+    elif age <= 19:
+        print("You are a Teen.")
+    elif age <= 64:
+        print ("You are an Adult.")
+    else:
+        print("You are a Senior")
+
+classify_age(101)
